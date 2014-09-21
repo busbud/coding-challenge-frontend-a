f919cd6a30b4471a43cfe2b1e1476cbf4c0bed70
Nebez Briefkani <me@nebezb.com>
2014-09-21 00:08:24 -0400

    Added localization, form error handling

--- public/js/app.js
+++ public/js/app.js
@@ -1,38 +1,140 @@
-/* global $, Bloodhound */
+/* global $, Bloodhound, document, window */
 
-// Typeahead > 0.9.3 requires bloodhound for remote requests, a bit annoying
-var cityResults = new Bloodhound({
-  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
-  queryTokenizer: Bloodhound.tokenizers.whitespace,
-  remote: {
-    url: '/search/%QUERY',
-    rateLimitWait: 150
+function initAutocomplete() {
+  // Typeahead > 0.9.3 requires bloodhound for remote requests, a bit annoying
+  var cityResults = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    remote: {
+      url: '/search/%QUERY',
+      rateLimitWait: 150
+    }
+  });
+
+  cityResults.initialize();
+
+
+  // Init typeahead
+  var autocompleteInputs = $('#depart-from,#arrive-at');
+  autocompleteInputs.typeahead({
+    minLength: 2,
+    hint: true
+  },
+  {
+    name: 'city',
+    displayKey: 'full_name',
+    source: cityResults.ttAdapter()
+  });
+
+  // Listen for selected events
+  autocompleteInputs.on('typeahead:selected typeahead:autocompleted', function(e, data) {
+    // Stick data.city_url into our data
+    $(this).data('cityUrl', data.city_url);
+  });
+
+  // Listen for manual (non-Typeahead) changes
+  autocompleteInputs.change(function() {
+    // Wipe the hcityUrl data
+    $(this).removeData('cityUrl');
+  });
+}
+
+function busSearch(e) {
+  // Non-JS browsers will have their form submitted. For every other browser,
+  // we'll take control
+  e.preventDefault();
+
+  // Are the data-city-url values populated?
+  var inputs = $('#depart-from,#arrive-at');
+  var errors = false;
+  var urls = [];
+
+  $.each(inputs, function() {
+    if(typeof $(this).data('cityUrl') === 'undefined' ||
+      $(this).data('cityUrl').length === 0)
+    {
+      // Display an alert and return
+      var errorMsg = $(this).parent().siblings('.error-message');
+      errorMsg.show(300);
+
+      // Close it after a couple seconds
+      setTimeout(function() { errorMsg.hide(300); }, 2500);
+
+      errors = true;
+    }
+    else
+    {
+      urls.push($(this).data('cityUrl'));
+    }
+  });
+
+  if(errors)
+  {
+    return;
   }
-});
 
-cityResults.initialize();
+  // Search busbud!
+  window.location = 'http://www.busbud.com/en/bus-schedules/' + urls[0] +'/' + urls[1];
+}
 
-$('#depart-from,#arrive-at').typeahead({
-  minLength: 2,
-  hint: true
-},
-{
-  name: 'city',
-  displayKey: 'full_name',
-  source: cityResults.ttAdapter()
-}).on('typeahead:selected typeahead:autocompleted', function(e, data) {
-  // Stick data.city_url into a hidden field
-  console.log('selected');
-});
+function translateElement(language, element) {
+  // Find out what we're translating (default to inner text)
+  var transKey = $(element).data('translate');
+  var transAttr = 'text';
 
-$('#depart-from,#arrive-at').change(function() {
-  console.log('changed');
-});
+  if(transKey.indexOf(':') !== -1)
+  {
+    var split = transKey.split(':');
+    transAttr =  split[0];
+    transKey = split[1];
+  }
 
-$('label.search-container input').focus(function() {
-  $(this).parents('label.search-container').addClass('active');
-});
+  if(transAttr === 'text')
+  {
+    $(element).text(language[transKey]);
+  }
+  else
+  {
+    $(element).attr(transAttr, language[transKey]);
+  }
+}
 
-$('label.search-container input').blur(function() {
-  $(this).parents('label.search-container').removeClass('active');
+function setLocale(lang, elements) {
+  // Clean out any hashes in lang and hit our API
+  var cleanLang = lang.replace('#', '').trim() || 'en';
+  $.ajax('/lang/' + cleanLang)
+    .done(function(res) {
+      // Iterate over the elements and translate them
+      $.each(elements, function(i, e) {
+        translateElement(res, e);
+      });
+
+      // Activate the current language
+      $('.languages a').removeClass('active');
+      $('.languages #' + cleanLang ).addClass('active');
+    });
+}
+
+$(document).ready(function() {
+  // Initialize the autocompletion functionality
+  initAutocomplete();
+
+  // Attach to the search form
+  $('#busbud-search').submit(busSearch);
+
+  // Set page locale on load and any time they click on a new language
+  var localizedElements = $('*[data-translate]');
+  setLocale(window.location.hash, localizedElements);
+  $('.languages a').click(function() {
+    setLocale($(this)[0].hash, localizedElements);
+  });
+
+  // Activate and deactivate input containers on focus/blur
+  $('label.search-container input').focus(function() {
+    $(this).parents('label.search-container').addClass('active');
+  });
+
+  $('label.search-container input').blur(function() {
+    $(this).parents('label.search-container').removeClass('active');
+  });
 });
