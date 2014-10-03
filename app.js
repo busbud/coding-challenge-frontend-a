5b4177d5f4aeb774f901f27aaea15d01dd7db4d4
Etienne Magnier <etienne.magnier@gmail.com>
2014-10-03 00:14:05 -0400

    Add a very very basic localization support

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /**
  * Module dependencies.
  */
@@ -5,31 +7,47 @@
 var express = require('express');
 var routes = require('./routes');
 var http = require('http');
-
 var app = express();
 
+// Add localization support
+var i18n = new (require('i18n-2'))({
+  locales: ['en', 'fr'],
+  extension: '.json'
+});
+app.locals.i18n = i18n;
+
 app.configure(function() {
-  'use strict';
   app.set('port', process.env.PORT || 5000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.use('/bower_components', express.static(__dirname + '/assets/bower_components'));
+  app.use(express.static(__dirname + '/dist'));
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use('/bower_components', express.static(__dirname + '/assets/bower_components'));
-  app.use(express.static(__dirname + '/dist'));
 });
 
 app.configure('development', function() {
-  'use strict';
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
+app
+  .get('/', routes.index)
+  .post('/', setLocale);
 
 http.createServer(app).listen(app.get('port'), function() {
-  'use strict';
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+// Very basic support of the localization switching
+function setLocale(req, res) {
+  var new_locale = req.param('locale');
+
+  if (new_locale) {
+    i18n.setLocale(new_locale);
+  }
+
+  res.redirect('/');
+}
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,5 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Test' });
+  res.render('index');
 };
