6b448841e07690a782ad35dd7b570937434aaf79
Etienne Magnier <etienne.magnier@gmail.com>
2014-10-05 18:38:37 -0400

    Makes jQuery.ajaxTransport working correctly on IE8/9

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,9 +1,17 @@
 /* jshint unused:false */
+'use strict';
+
 
 // Global Dependencies
 var $ = require('jquery');
 var _ = require('lodash');
 var React = window.React = require('react');
+
+
+// Makes jQuery.ajaxTransport working correctly on IE8/9
+// More info here: http://stackoverflow.com/a/10232313/893357
+require('jquery-ajax-hooks-xdr');
+
 
 // Modules
 require('./modules/citySearchForm.js');
