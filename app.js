2cf5abc114de63741a1325672154427a99b4c4a8
Guillaume <trivial.tide@gmail.com>
2014-10-27 23:36:42 -0400

    Add POST handling on server (values are logged in the console) Upgrade CSS and look Add support for disabled javascript

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
-//app.get('/GetCity', routes.GetCity);
+app.post('/search', routes.search);
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,11 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Test' });
+  res.render('index', { title: 'Schedule and tickets for worldwide bus travel' });
+};
+
+exports.search = function(req, res){
+    console.log('From:' + req.body.SearchViewFrom);
+    console.log('To: ' + req.body.SearchViewTo);
+    res.render('search', { title: 'Search page' });
 };
