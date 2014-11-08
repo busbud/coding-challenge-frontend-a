18b902eb9fe7b4645fddecfaa1830788fa8a0ec2
Tristan Tran <tristan.tran89@gmail.com>
2014-11-07 19:39:48 -0500

    Submitted first version of solution

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.configure(function(){
   app.set('port', process.env.PORT || 5000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.use(express.static(path.join(__dirname, 'public')));
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
