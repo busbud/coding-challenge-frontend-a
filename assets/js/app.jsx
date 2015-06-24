be4a76d6d3f5142e030e4d7110cf2734770e2948
Angus MacIsaac <donaldamacisaac@gmail.com>
2015-06-24 14:00:44 -0400

    Add functionality for displaying error message if form has not been filled properly.

--- assets/js/app.jsx
+++ assets/js/app.jsx
@@ -15,45 +15,21 @@ function mapLocationsToSelectOptions(locations) {
 var BLocationBox = React.createClass({
   loadBLocationsFromServer: function(inputText, callback) {
     var url = this.props.url + '{' + inputText + '}';
-    console.log(url);
 
     $.ajax({
       url: url,
       dataType: 'json',
       success: function(data) {
-        console.log(mapLocationsToSelectOptions(data));
         callback(null, {options: mapLocationsToSelectOptions(data)})
       }.bind(this),
       error: function(xhr, status, err) {
-        console.log('error');
         console.error(this.props.url, status, err.toString());
       }.bind(this)
     });
   },
 
   handleBLocationSubmit: function(location) {
-    var locations = this.state.data;
-
-    locations.push(location);
-
-    this.setState({data: locations}, function() {
-      $.ajax({
-        url: this.props.url,
-        dataType: 'json',
-        type: 'POST',
-        data: location,
-        success: function(data) {
-          this.setState({data: data});
-        }.bind(this),
-        error: function(xhr, status, err) {
-          console.error(this.props.url, status, err.toString());
-        }.bind(this)
-      });
-    });
-  },
-
-  getInitialState: function() {
-    return {data: []};
+    console.log('Location submitted.');
   },
 
   render: function() {
@@ -66,47 +42,65 @@ var BLocationBox = React.createClass({
 });
 
 var BLocationForm = React.createClass({
+  getInitialState: function() {
+    return {
+      locationFrom: null,
+      locationTo: null,
+      errors: false,
+    }
+  },
+
+  onFromChange: function(value) {
+    this.setState({ locationFrom: value, errors: false });
+  },
+
+  onToChange: function(value) {
+    this.setState({ locationTo: value, errors: false });
+  },
+
   handleSubmit: function(e) {
     e.preventDefault();
 
-    var author = React.findDOMNode(this.refs.author).value.trim();
-    var text = React.findDOMNode(this.refs.text).value.trim();
-
-    if (!text || !author) {
+    if (!this.state.locationFrom || !this.state.locationTo) {
+      this.setState({ errors: true });
       return;
     }
 
-    this.props.onBLocationSubmit({author: author, text: text});
-
-    React.findDOMNode(this.refs.author).value = '';
-    React.findDOMNode(this.refs.text).value = '';
+    this.props.onBLocationSubmit(this.state);
   },
 
   render: function() {
-    var setPlaceholder = function setPlaceholder(event) {
-    }
-
     return (
-      <form className="form" onSubmit={this.handleSubmit}>
-        <Select
-            name="form-field-name"
-            placeholder="Leaving from"
-            multi={false}
-            asyncOptions={this.props.getOptions}
-            onFocus={setPlaceholder}
-        />
-        <Select
-            name="form-field-name"
-            placeholder=""
-            multi={false}
-            placeholder="Going to"
-            asyncOptions={this.props.getOptions}
-            onFocus={setPlaceholder}
-        />
-        <button type="submit" className="btn-primary">
-          <i className="fa fa-search"></i> Search
-        </button>
-      </form>
+      <div className="form-container">
+        <form className={this.state.errors ? 'form errors' : 'form'} onSubmit={this.handleSubmit}>
+          <div className="select-group">
+            <i className="fa fa-location-arrow select-group-addon"></i>
+            <Select name="location-from"
+              placeholder="Leaving from"
+              value={this.state.locationFrom}
+              multi={false}
+              asyncOptions={this.props.getOptions}
+              onChange={this.onFromChange}>
+            </Select>
+          </div>
+          <div className="select-group">
+            <i className="fa fa-map-marker select-group-addon"></i>
+            <Select name="location-to"
+              placeholder=""
+              value={this.state.locationTo}
+              multi={false}
+              placeholder="Going to"
+              asyncOptions={this.props.getOptions}
+              onChange={this.onToChange}/>
+          </div>
+          <button type="submit" className="btn-primary">
+            <i className="fa fa-search"></i> Search
+          </button>
+        </form>
+        <div className={this.state.errors ? 'form-errors' : 'displayNone'}>
+          <span>Please enter a departure and arrival location!</span>
+        </div>
+      </div>
     );
   }
 });
